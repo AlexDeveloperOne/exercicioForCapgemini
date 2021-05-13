16a51f8b58b592a36bdb5c340087c408ac16003a
Alexandre A. Silva <57747390+AlexDeveloperOne@users.noreply.github.com>
2021-05-12 21:07:39 -0300

    Create printfExample.c

---
+++ Basic/printfExample.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+   clrscr();
+	printf("\tMy Introduction\n");
+	printf("Hey Friends I m Ram\n");
+	printf("I m student of BeTheDeveloper.com");
+	printf("\nC programming is best for me.");
+	printf("\t");
+   getch();
+}
